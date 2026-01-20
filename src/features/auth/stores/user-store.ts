e987d94b0a08b2125d105bc57e22b76c59f100d8
Vinícius <vinicius@email.com>
2026-01-20 16:49:13 -0300

    feat(auth): creat user and auth stores

---
+++ src/features/auth/stores/user-store.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+type User = {
+  id: string;
+  fullName: string;
+  email: string;
+  birthDate: Date;
+};
+
+type UserState = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
+
+export const useUserStore = create<UserState>(set => ({
+  user: null,
+  setUser: user => set({ user }),
+}));
