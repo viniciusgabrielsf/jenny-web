b5a47b6e282b48b7980f78602e7f579ef8f35dbd
Vinícius <vinicius@email.com>
2026-01-20 21:39:23 -0300

    test: config zustand mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
+/// <reference types="vitest/globals" />
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -41,6 +43,7 @@ export default defineConfig(({ mode }) => {
     },
     test: {
       environment: 'jsdom',
+      globals: true,
       setupFiles: './src/test/setup.ts',
     },
   };
