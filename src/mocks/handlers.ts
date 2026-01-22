c89b7b730dfbab439ec439089821d6398dfd050a
Vinícius <vinicius@email.com>
2026-01-22 16:52:12 -0300

    refactor: padronize API mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,14 +4,17 @@ export const API_URL = 'http://localhost:5000/api';
 
 export const handlers = [
   http.post(`${API_URL}/users`, () => new HttpResponse(null, { status: 201 })),
-  http.post(`${API_URL}/auth/login`, () =>
-    HttpResponse.json({
-      user: {
-        id: '1',
-        fullName: 'Killua Zoldyck',
-        email: 'killua.zoldyck@email.com',
-        birthDate: '10-10-2000',
-      },
-    })
+  http.post(
+    `${API_URL}/auth/login`,
+    () =>
+      new HttpResponse(
+        {
+          id: '1',
+          fullName: 'Killua Zoldyck',
+          email: 'killua.zoldyck@email.com',
+          birthDate: '10-10-2000',
+        },
+        { status: 200 }
+      )
   ),
 ];
