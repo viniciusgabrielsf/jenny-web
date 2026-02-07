807a9500e063b606dda0834864290d53006075a7
Vinícius <vinicius@email.com>
2026-02-06 21:09:03 -0300

    refactor: remove unused import

--- src/features/auth/pages/__test__/sign-up.test.tsx
+++ src/features/auth/pages/__test__/sign-up.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
-import { render, screen, waitFor } from '@test/utils';
+import { render, screen } from '@test/utils';
 import userEvent from '@testing-library/user-event';
 import { SignUpPage } from '../sign-up';
 import { server } from '../../../../mocks/server';
