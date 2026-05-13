e6fc6522a8a4b5a456a3d1d0ffa956ac3246febd
Vinícius <vigasife@gmail.com>
2026-05-13 13:15:02 -0300

    team: build edit team modal

---
+++ src/features/teams/hooks/use-edit-team.ts
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { teamsClient, type EditTeamRequest } from '../api/teams-client';
+
+export const useEditTeam = () => {
+  const editTeamMutation = useMutation({
+    mutationKey: ['edit-team'],
+    mutationFn: async (request: EditTeamRequest) => {
+      toast.loading('Editando time...', { id: 'edit-team-loading' });
+
+      return teamsClient.editTeam(request);
+    },
+    onSuccess: () => {
+      toast.dismiss('edit-team-loading');
+      toast.success('Time editado com sucesso!', { id: 'edit-team-success' });
+    },
+    onError: (error: Error) => {
+      toast.dismiss('edit-team-loading');
+      toast.error(`Erro ao editar time: ${error.message}`, { id: 'edit-team-error' });
+    },
+  });
+
+  return {
+    editTeam: editTeamMutation,
+  };
+};
